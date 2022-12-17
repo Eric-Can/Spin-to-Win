e9a9f1528d0bb05f669203d313d7c77152b21735
Eric-Can <canerse.ec@gmail.com>
2022-12-17 13:10:06 -0600

    initial project setup

---
+++ web/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HomePage from "./shared/pages/HomePage";
+
+const App = () => {
+	return <HomePage />;
+};
+
+const root = ReactDOM.createRoot(
+	document.getElementById("root") as HTMLElement
+);
+root.render(<App />);
+a;
